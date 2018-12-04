654aa7eea4f8649f8e54eafc0db74a61dd7e246e
Gabriel Pinto <gl.pinto10@uniandes.edu.co>
2018-12-04 12:38:18 -0500

    Configurations for tests with imports

--- imports/ui/components/Navbar.test.jsx
+++ imports/ui/components/Navbar.test.jsx
@@ -1,17 +1,20 @@
-// import React from 'react';
-// import chai from 'chai';
-// import { Meteor } from 'meteor/meteor';
-//import Navbar from './Navbar.jsx';
-// import Adapter from 'enzyme-adapter-react-16';
-// import { shallow, configure } from 'enzyme';
+import React from 'react';
+import chai from 'chai';
+import { Meteor } from 'meteor/meteor';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 
-// configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
+if (Meteor.isServer) return; //client-only
 
-// describe('Navbar', () => {
-//   if (Meteor.isServer) return; //client-only
-//   describe('Navbar', () => {
-//     it('Should render navbar', () => {
-//       let navbar = shallow(<Navbar />);
-//     });
-//   });
-// });
+if (Meteor.isClient) {
+  import Navbar from './Navbar.jsx';
+
+  describe('Navbar', () => {
+    describe('Navbar', () => {
+      it('Should render navbar', () => {
+        let navbar = shallow(<Navbar />);
+      });
+    });
+  });
+}
